8fe52fccdfc78a428d96dc9d8c595f772ad44e3c
Ilian Iliev <ilian@ilian.io>
2019-01-04 14:47:13 +0200

    Downloading latest free maxmind DB on install

--- geolite2/setup.py
+++ geolite2/setup.py
@@ -1,9 +1,29 @@
 import os
+from distutils.command.build_py import build_py
 from setuptools import setup
+from shutil import copyfile
+import subprocess
 
 with open(os.path.join(os.path.dirname(__file__),
                        'VERSION')) as f:
     version = f.read().strip()
+
+
+def download_db():
+    subprocess.call(['./download.sh'])
+
+
+class my_build_py(build_py):
+    def run(self):
+        build_py.run(self)
+
+        if not self.dry_run:
+            # Downlaod DB and copy it to the lib folder so it is later installed with the rest
+            download_db()
+            target_dir = os.path.join(self.build_lib)
+
+            path = '_geoip_geolite2/GeoLite2-City.mmdb'
+            copyfile(path, os.path.join(target_dir, path))
 
 
 setup(
@@ -22,4 +42,7 @@ setup(
     classifiers=[
         'Programming Language :: Python',
     ],
+    cmdclass={
+        'build_py': my_build_py
+    }
 )
